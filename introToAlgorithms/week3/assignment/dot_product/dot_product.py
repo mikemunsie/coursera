0942d9621537148b7ddd834ae2f3a98c2f2c58c7
Mike Munsie <mmunsie@researchnow.com>
2017-05-20 00:44:24 -0500

    Tried to solve the fractional knapsack, but couldn't get past the tester :/ - Instead completed the dot product one. It was... way easier lol

--- introToAlgorithms/week3/assignment/dot_product/dot_product.py
+++ introToAlgorithms/week3/assignment/dot_product/dot_product.py
@@ -3,8 +3,9 @@
 import sys
 
 def max_dot_product(a, b):
-    #write your code here
     res = 0
+    a.sort()
+    b.sort()
     for i in range(len(a)):
         res += a[i] * b[i]
     return res
@@ -16,4 +17,4 @@ if __name__ == '__main__':
     a = data[1:(n + 1)]
     b = data[(n + 1):]
     print(max_dot_product(a, b))
-    
+
--- introToAlgorithms/week3/assignment/fractional_knapsack/fractional_knapsack.py
+++ introToAlgorithms/week3/assignment/fractional_knapsack/fractional_knapsack.py
@@ -1,46 +1,33 @@
 # Uses python3
 import sys
 
+# I've attempted this assignment multiple times and still get an out of bounds issue...
+# This shouldn't be happening from what I can tell... moving on to a different problem.
+
 def get_optimal_value(capacity, weights, values):
     knapsackValue = float(0)
     ratios = []
 
     # Sort item by the most valuable per quantity
     for index, value in enumerate(values):
-      if index >= len(weights):
-        break;
-      ratios.append((float(weights[index])/float(value), float(value), float(weights[index])))
-
-    # If no ratios have been created, break early
-    if len(ratios) == 0:
-      return 0
-
-    ratios.sort(key=lambda tup: tup[0])
+      ratios.append((1.0 * weights[index]/values[index], weights[index], values[index]))
+    ratios.sort(key=lambda x: x[0])
 
     # Continue picking items until our capacity has been met
-    while len(ratios) > 0:
-
-      # If the entire item fits, use it
-      if capacity - ratios[0][2] > 0:
-        capacity-= ratios[0][2]
-        knapsackValue+= ratios[0][1]
-        ratios.pop(0)
-
-      # Otherwise take as much as we can from it (we can also short circuit)
-      else:
-        knapsackValue+= (capacity / ratios[0][2]) * ratios[0][1]
-        break
+    for ratio in ratios:
+      amount = min(ratio[1], capacity)
+      capacity-=amount
+      knapsackValue+= 1.0 * amount / ratio[1] * ratio[2]
+      if (capacity <= 0): break
 
     return knapsackValue
 
 
 if __name__ == "__main__":
-    '''
     data = list(map(int, sys.stdin.read().split()))
     n, capacity = data[0:2]
     values = data[2:(2 * n + 2):2]
     weights = data[3:(2 * n + 2):2]
     opt_value = get_optimal_value(capacity, weights, values)
-    '''
-    opt_value = get_optimal_value(50, [10,20], [20, 30])
+    #opt_value = get_optimal_value(50, [20,50,30], [60, 100, 120])
     print("{:.10f}".format(opt_value))
