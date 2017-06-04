c41d8a89f9cb562c8e8e5412fd46694e9539cea5
Mike Munsie <mmunsie@researchnow.com>
2017-06-04 17:48:07 -0500

    WIP

--- dataStructures/week1/assignment/tree_height/tree-height.py
+++ dataStructures/week1/assignment/tree_height/tree-height.py
@@ -1,25 +1,70 @@
 # python3
+#
+'''
+Ex:
+6
+1 -1 0 0 1 2
+
+Node 0 is a child of Node 1
+Node 1 is the root
+Node 2 is a child of Node 0
+Node 3 is a child of Node 0
+Node 4 is a child of Node 1
+Node 5 is a child of Node 2
+
+                1
+               / \
+              0   4
+              | \
+              2  3
+              |
+              5
+
+'''
 
 import sys, threading
 sys.setrecursionlimit(10**7) # max depth of recursion
 threading.stack_size(2**27)  # new thread will get stack of such size
 
-class TreeHeight:
-        def read(self):
-                self.n = int(sys.stdin.readline())
-                self.parent = list(map(int, sys.stdin.readline().split()))
+class Node:
+    def __init__(self):
+        self.children = []
+        self.parents = []
+    def addChild(self, node):
+        self.children.append(node)
+    def addParent(self, node):
+        self.parents.append(node)
 
-        def compute_height(self):
-                # Replace this code with a faster implementation
-                maxHeight = 0
-                for vertex in range(self.n):
-                        height = 0
-                        i = vertex
-                        while i != -1:
-                                height += 1
-                                i = self.parent[i]
-                        maxHeight = max(maxHeight, height);
-                return maxHeight;
+class TreeHeight:
+    def read(self):
+        #self.n = int(sys.stdin.readline())
+        #self.parent = list(map(int, sys.stdin.readline().split()))
+        self.n = 5
+        self.parent = list(map(int, [4,-1,4,1,1]))
+
+    def compute_height(self):
+        nodes = []
+        root = -1
+        maxHeight = 1
+
+        # Create the nodes based on the size of the tree
+        for node in range(self.n):
+            nodes.append(Node())
+        for childIndex in range(self.n):
+            parentIndex = self.parent[childIndex]
+            if parentIndex == -1:
+                root = childIndex
+            else:
+                nodes[parentIndex].addChild(nodes[childIndex])
+                nodes[childIndex].addParent(nodes[parentIndex])
+
+        for nodeIndex in range(self.n):
+            height = 0
+            parents = nodes[nodeIndex].parents
+            while len(parents) > 0:
+                parents = parents[0].parents
+                maxHeight = max(height, maxHeight)
+        return maxHeight
 
 def main():
   tree = TreeHeight()
